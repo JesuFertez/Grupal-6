ab6980ee0ee77d577d3139b1a50c5e95627d7e2a
María Fernández <83524258+JesuFertez@users.noreply.github.com>
2023-07-27 21:37:34 -0400

    Add files via upload
    Avance proyecto grupal 6

---
+++ GrupalJpa/src/main/java/cl/grupo2/GrupalJpa/controller/HomeController.java
@@ -0,0 +1,17 @@
+package cl.grupo2.GrupalJpa.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+    public ModelAndView mostrarInicio() {
+        return new ModelAndView("index");
+    }
+}
